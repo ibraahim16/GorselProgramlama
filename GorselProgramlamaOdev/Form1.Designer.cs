5b591ea6af67ff7cff8cb9dfe12be7542eb2346a
Ibrahim Seyrek <iseyrek@icloud.com>
2025-04-16 13:44:45 +0300

    Add project files.

---
+++ GorselProgramlamaOdev/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace GorselProgramlamaOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.cmbGun = new System.Windows.Forms.ComboBox();
+            this.cmbAy = new System.Windows.Forms.ComboBox();
+            this.cmbYil = new System.Windows.Forms.ComboBox();
+            this.txtKilo = new System.Windows.Forms.TextBox();
+            this.txtBoy = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblVKI = new System.Windows.Forms.Label();
+            this.lblBurc = new System.Windows.Forms.Label();
+            this.lblVKIYorum = new System.Windows.Forms.Label();
+            this.lblBurcYorum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(247, 590);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(142, 69);
+            this.btnKaydet.TabIndex = 0;
+            this.btnKaydet.Text = "Hesapla";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(247, 397);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(76, 57);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(117, 21);
+            this.txtAd.TabIndex = 3;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(76, 92);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(117, 21);
+            this.txtSoyad.TabIndex = 4;
+            // 
+            // cmbGun
+            // 
+            this.cmbGun.FormattingEnabled = true;
+            this.cmbGun.Location = new System.Drawing.Point(65, 163);
+            this.cmbGun.Name = "cmbGun";
+            this.cmbGun.Size = new System.Drawing.Size(53, 23);
+            this.cmbGun.TabIndex = 5;
+            // 
+            // cmbAy
+            // 
+            this.cmbAy.FormattingEnabled = true;
+            this.cmbAy.Location = new System.Drawing.Point(165, 163);
+            this.cmbAy.Name = "cmbAy";
+            this.cmbAy.Size = new System.Drawing.Size(53, 23);
+            this.cmbAy.TabIndex = 6;
+            // 
+            // cmbYil
+            // 
+            this.cmbYil.FormattingEnabled = true;
+            this.cmbYil.Location = new System.Drawing.Point(275, 163);
+            this.cmbYil.Name = "cmbYil";
+            this.cmbYil.Size = new System.Drawing.Size(53, 23);
+            this.cmbYil.TabIndex = 7;
+            // 
+            // txtKilo
+            // 
+            this.txtKilo.Location = new System.Drawing.Point(101, 240);
+            this.txtKilo.Name = "txtKilo";
+            this.txtKilo.Size = new System.Drawing.Size(117, 21);
+            this.txtKilo.TabIndex = 8;
+            // 
+            // txtBoy
+            // 
+            this.txtBoy.Location = new System.Drawing.Point(101, 200);
+            this.txtBoy.Name = "txtBoy";
+            this.txtBoy.Size = new System.Drawing.Size(117, 21);
+            this.txtBoy.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 29);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 29);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Dogum Tarihi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 29);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Gün:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(124, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 29);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ay:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(230, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 29);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Yıl:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(7, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 29);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Kilo (kg):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(7, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 29);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Boy (m):";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(4, 276);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 29);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Burç:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(7, 515);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(131, 29);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Burç Yorumu:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(255, 331);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 29);
+            this.label11.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(4, 317);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 29);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "VKI:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(4, 365);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(121, 29);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "VKI Yorumu:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Gill Sans MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(242, 365);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(116, 29);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "Burç Resmi:";
+            // 
+            // lblVKI
+            // 
+            this.lblVKI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblVKI.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.14F);
+            this.lblVKI.Location = new System.Drawing.Point(76, 317);
+            this.lblVKI.Name = "lblVKI";
+            this.lblVKI.Size = new System.Drawing.Size(122, 29);
+            this.lblVKI.TabIndex = 24;
+            // 
+            // lblBurc
+            // 
+            this.lblBurc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBurc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.14F);
+            this.lblBurc.Location = new System.Drawing.Point(76, 276);
+            this.lblBurc.Name = "lblBurc";
+            this.lblBurc.Size = new System.Drawing.Size(122, 29);
+            this.lblBurc.TabIndex = 25;
+            // 
+            // lblVKIYorum
+            // 
+            this.lblVKIYorum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblVKIYorum.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.14F);
+            this.lblVKIYorum.Location = new System.Drawing.Point(9, 394);
+            this.lblVKIYorum.Name = "lblVKIYorum";
+            this.lblVKIYorum.Size = new System.Drawing.Size(209, 106);
+            this.lblVKIYorum.TabIndex = 26;
+            // 
+            // lblBurcYorum
+            // 
+            this.lblBurcYorum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBurcYorum.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.14F);
+            this.lblBurcYorum.Location = new System.Drawing.Point(12, 544);
+            this.lblBurcYorum.Name = "lblBurcYorum";
+            this.lblBurcYorum.Size = new System.Drawing.Size(206, 125);
+            this.lblBurcYorum.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 690);
+            this.Controls.Add(this.lblBurcYorum);
+            this.Controls.Add(this.lblVKIYorum);
+            this.Controls.Add(this.lblBurc);
+            this.Controls.Add(this.lblVKI);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoy);
+            this.Controls.Add(this.txtKilo);
+            this.Controls.Add(this.cmbYil);
+            this.Controls.Add(this.cmbAy);
+            this.Controls.Add(this.cmbGun);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnKaydet);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.12F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.ComboBox cmbGun;
+        private System.Windows.Forms.ComboBox cmbAy;
+        private System.Windows.Forms.ComboBox cmbYil;
+        private System.Windows.Forms.TextBox txtKilo;
+        private System.Windows.Forms.TextBox txtBoy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblVKI;
+        private System.Windows.Forms.Label lblBurc;
+        private System.Windows.Forms.Label lblVKIYorum;
+        private System.Windows.Forms.Label lblBurcYorum;
+    }
+}
+
---
+++ GorselProgramlamaOdev/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GorselProgramlamaOdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnKaydet.Click += btnKaydet_Click; // ← bu satır butonu bağlar
+        }
+
+
+
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            // Giriş verileri
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            int gun = Convert.ToInt32(cmbGun.SelectedItem);
+            string ay = cmbAy.SelectedItem.ToString();
+            int yil = Convert.ToInt32(cmbYil.SelectedItem);
+            
+            double boy = Convert.ToDouble(txtBoy.Text.Replace(",", "."),
+                              System.Globalization.CultureInfo.InvariantCulture);
+            double kilo = Convert.ToDouble(txtKilo.Text.Replace(",", "."),
+                                           System.Globalization.CultureInfo.InvariantCulture);
+
+
+            // VKİ hesaplama
+            double vki = kilo / (boy * boy);
+            string vkiYorum = "";
+
+            if (vki < 18.5)
+                vkiYorum = "Zayıf";
+            else if (vki < 25)
+                vkiYorum = "Normal";
+            else if (vki < 30)
+                vkiYorum = "Fazla kilolu";
+            else
+                vkiYorum = "Obez";
+
+            // Burç hesaplama
+            string burc = "";
+            string burcYorum = "";
+            string burcResmi = "";
+
+            if ((ay == "Ocak" && gun >= 22) || (ay == "Şubat" && gun <= 19))
+            {
+                burc = "Kova";
+                burcYorum = "Bağımsız ve yaratıcı.";
+                burcResmi = "Images/kova.png";
+            }
+            else if ((ay == "Şubat" && gun >= 20) || (ay == "Mart" && gun <= 20))
+            {
+                burc = "Balık";
+                burcYorum = "Hayalperest ve sezgiseldir.";
+                burcResmi = "Images/balik.png";
+            }
+            else if ((ay == "Mart" && gun >= 21) || (ay == "Nisan" && gun <= 20))
+            {
+                burc = "Koç";
+                burcYorum = "Lider ruhlu ve enerjik.";
+                burcResmi = "Images/koc.png";
+            }
+            else if ((ay == "Nisan" && gun >= 21) || (ay == "Mayıs" && gun <= 21))
+            {
+                burc = "Boğa";
+                burcYorum = "Sadık ve kararlıdır.";
+                burcResmi = "Images/boga.png";
+            }
+            else if ((ay == "Mayıs" && gun >= 22) || (ay == "Haziran" && gun <= 22))
+            {
+                burc = "İkizler";
+                burcYorum = "Meraklı ve konuşkandır.";
+                burcResmi = "Images/ikizler.png";
+            }
+            else if ((ay == "Haziran" && gun >= 23) || (ay == "Temmuz" && gun <= 22))
+            {
+                burc = "Yengeç";
+                burcYorum = "Duygusal ve koruyucudur.";
+                burcResmi = "Images/yengec.png";
+            }
+            else if ((ay == "Temmuz" && gun >= 23) || (ay == "Ağustos" && gun <= 22))
+            {
+                burc = "Aslan";
+                burcYorum = "Kendine güvenen ve lider ruhlu.";
+                burcResmi = "Images/aslan.png";
+            }
+            else if ((ay == "Ağustos" && gun >= 23) || (ay == "Eylül" && gun <= 22))
+            {
+                burc = "Başak";
+                burcYorum = "Titiz ve detaycıdır.";
+                burcResmi = "Images/basak.png";
+            }
+            else if ((ay == "Eylül" && gun >= 23) || (ay == "Ekim" && gun <= 22))
+            {
+                burc = "Terazi";
+                burcYorum = "Adil ve uyumlu.";
+                burcResmi = "Images/terazi.png";
+            }
+            else if ((ay == "Ekim" && gun >= 23) || (ay == "Kasım" && gun <= 21))
+            {
+                burc = "Akrep";
+                burcYorum = "Gizemli ve tutkulu.";
+                burcResmi = "Images/akrep.png";
+            }
+            else if ((ay == "Kasım" && gun >= 22) || (ay == "Aralık" && gun <= 21))
+            {
+                burc = "Yay";
+                burcYorum = "Gezmeyi ve özgürlüğü sever.";
+                burcResmi = "Images/yay.png";
+            }
+            else if ((ay == "Aralık" && gun >= 22) || (ay == "Ocak" && gun <= 21))
+            {
+                burc = "Oğlak";
+                burcYorum = "Çalışkan ve disiplinlidir.";
+                burcResmi = "Images/oglak.png";
+            }
+
+            // Sonuçları göster
+            lblVKI.Text = vki.ToString("F2");
+            lblVKIYorum.Text = vkiYorum;
+            lblBurc.Text = burc;
+            lblBurcYorum.Text = burcYorum;
+            pictureBox1.ImageLocation = burcResmi;
+
+
+
+
+
+            // Veritabanına kayıt işlemi
+            try
+            {
+                MessageBox.Show("Kayıt işlemi başladı...");
+                string connStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GorselVeritabani;Integrated Security=True";
+                SqlConnection con = new SqlConnection(connStr);
+
+
+                con.Open();
+
+                string sql = "INSERT INTO Kisiler (Ad, Soyad, Gun, Ay, Yil, Boy, Kilo, VKI, VKIYorum, Burc, BurcYorum, BurcResmi) " +
+                             "VALUES (@Ad, @Soyad, @Gun, @Ay, @Yil, @Boy, @Kilo, @VKI, @VKIYorum, @Burc, @BurcYorum, @BurcResmi)";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Ad", ad);
+                cmd.Parameters.AddWithValue("@Soyad", soyad);
+                cmd.Parameters.AddWithValue("@Gun", gun);
+                cmd.Parameters.AddWithValue("@Ay", ay);
+                cmd.Parameters.AddWithValue("@Yil", yil);
+                cmd.Parameters.AddWithValue("@Boy", boy);
+                cmd.Parameters.AddWithValue("@Kilo", kilo);
+                cmd.Parameters.AddWithValue("@VKI", vki);
+                cmd.Parameters.AddWithValue("@VKIYorum", vkiYorum);
+                cmd.Parameters.AddWithValue("@Burc", burc);
+                cmd.Parameters.AddWithValue("@BurcYorum", burcYorum);
+                cmd.Parameters.AddWithValue("@BurcResmi", burcResmi);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Kullanıcı veritabanına kaydedildi ✅");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veritabanı Hatası ❌: " + ex.Message);
+            }
+
+        }
+        private void btnKaydet_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Günleri ekle (1–31)
+            for (int i = 1; i <= 31; i++)
+                cmbGun.Items.Add(i);
+
+            // Ayları ekle
+            string[] aylar = {
+        "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+        "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
+    };
+            cmbAy.Items.AddRange(aylar);
+
+            // Yılları ekle (1960–2025)
+            for (int i = 1960; i <= 2025; i++)
+                cmbYil.Items.Add(i);
+        }
+
+       
+    }
+}
